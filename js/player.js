3bc13239a0b2c10550ca8b74e1d6aa1e8c730a50
andreagennaioli <andrea.gennaioli@hotmail.it>
2022-12-05 18:59:55 +0100

    hot fix 1

--- js/player.js
+++ js/player.js
@@ -1,3 +1,21 @@
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 var playList = [
   './songs/Jingle Bell Rock Remix (A Trappy Christmas).mp3',
   './songs/LET IT SNOW! LET IT SNOW! (Trap Remix).mp3',
@@ -9,6 +27,7 @@ var playList = [
   './songs/SANTA CLAUS IS COMING TO TOWN (OFFICIAL Dance Remix).mp3',
   './songs/Rockin\' Around The Christmas Tree (Christmas Trap Remix).mp3'
 ];
+shuffle(playList);
 var audio = new Audio(playList[0]);
 audio.onended = () => {
   setTimeout(() => {
@@ -17,6 +36,9 @@ audio.onended = () => {
 }
 audio.pause()
 audio.volume = 0.06;
+audio.id = 'music-player'
+audio.style.display = 'none'
+document.body.appendChild(audio)
 document.getElementById('player-name').innerHTML = playList[0].replace('./songs/', '')
 var index = 0;
 var playing = false;
@@ -30,63 +52,54 @@ function validateBackButton() {
 }
 
 document.getElementById('player-play').addEventListener('click', (e) => {
+  console.log('play click')
   if (playing) {
     playing = false;
     e.target.setAttribute('src', 'play.svg')
-    audio.pause();
+    document.getElementById('music-player').pause();
   } else {
     playing = true;
     e.target.setAttribute('src', 'stop.svg')
-    audio.play();
+    document.getElementById('music-player').play();
   }
 });
 
 document.getElementById('player-back').addEventListener('click', (e) => {
   if(document.getElementById('player-back').classList.contains('disabled')) return;
 
-  audio.pause()
+  document.getElementById('music-player').pause()
   index--;
-  audio = new Audio(playList[index]);
-  audio.onended = () => {
-    setTimeout(() => {
-      onend();
-    }, 1000)
-  }
+  document.getElementById('music-player').setAttribute('src', playList[index])
+  document.getElementById('music-player').load()
   document.getElementById('player-name').innerHTML = playList[index].replace('./songs/', '')
-  audio.volume = 0.06;
   validateBackButton();
-  setInterval(() => {
-    audio.play();
+  setTimeout(() => {
+    playing = true;
+    document.getElementById('player-play').setAttribute('src', 'stop.svg')
+    document.getElementById('music-player').play();
   }, 500)
 })
 
 document.getElementById('player-next').addEventListener('click', (e) => {
-  audio.pause()
+  document.getElementById('music-player').pause()
   index = index == playList.length - 1 ? 0 : index + 1;
-  audio = new Audio(playList[index]);
-  audio.onended = () => {
-    setTimeout(() => {
-      onend();
-    }, 1000)
-  }
+  document.getElementById('music-player').setAttribute('src', playList[index])
+  document.getElementById('music-player').load()
   document.getElementById('player-name').innerHTML = playList[index].replace('./songs/', '')
-  audio.volume = 0.06;
   validateBackButton();
-  setInterval(() => {
-    audio.play();
+  setTimeout(() => {
+    playing = true;
+    document.getElementById('player-play').setAttribute('src', 'stop.svg')
+    document.getElementById('music-player').play();
   }, 500)
 });
 
 function onend() {
+  console.log('ending')
   index = index == playList.length - 1 ? 0 : index + 1;
-  audio = new Audio(playList[index]);
-  audio.onended = () => {
-    setTimeout(() => {
-      onend();
-    }, 1000)
-  }
+  document.getElementById('music-player').setAttribute('src', playList[index])
+  document.getElementById('music-player').load()
+  document.getElementById('music-player').play()
   document.getElementById('player-name').innerHTML = playList[index].replace('./songs/', '')
-  audio.volume = 0.06;
   validateBackButton();
-  audio.play();
 }
