7c7548f4a4cb64a6faf0d6e7b86e900e694ce7b2
andreagennaioli <andrea.gennaioli@hotmail.it>
2023-03-21 21:36:49 +0100

    fix

--- js/timer.js
+++ js/timer.js
@@ -23,6 +23,8 @@ function timer() {
 		let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 		let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 		let seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+		days = (days < 10) ? "0" + days : days;
 		hours = (hours < 10) ? "0" + hours : hours;
 		minutes = (minutes < 10) ? "0" + minutes : minutes;
 		seconds = (seconds < 10) ? "0" + seconds : seconds;
